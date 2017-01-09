019d81cd3b940b411b13e2bed21d34795adb5790
Finn Dorph-Petersen <finnd@wayf.dk>
2017-01-09 08:03:20 +0100

    Cleanup and fix the unit test code

--- lMDQupdate_test.go
+++ lMDQupdate_test.go
@@ -9,6 +9,11 @@ import (
 )
 
 var SSLmodulusHash string = "3c9a81a80e9032f888ba3cc7ac564364c38f283e"
+
+// var schemaPath string
+const schemaPath string = "vendor/github.com/wayf-dk/gosaml/schemas/ws-federation.xsd"
+const wrongSchema string = "vendor/github.com/wayf-dk/gosaml/schemas/saml-schema-protocol-2.0.xsd"
+
 var metadata []byte
 
 // =======================================================
@@ -22,16 +27,18 @@ func TestMain(m *testing.M) {
 }
 
 func TestValidateMetadataNotValidDocument(t *testing.T) {
-	err := validateMetadata(metadata[:30], "src/github.com/wayf-dk/gosaml/schemas/saml-schema-protocol-2.0.xsd", SSLmodulusHash)
+	err := validateMetadata(metadata[:30], schemaPath, SSLmodulusHash)
 	if err.Error() != "Document validation error -1" {
-		t.Error("Document not validation failed")
+		t.Errorf("Document not validation failed '%s'", err)
 	}
 }
 
-func TestValidateMetadataNotValidSchema(t *testing.T) {
-	err := validateMetadata(metadata, "src/github.com/wayf-dk/gosaml/schemas/saml-schema-protocol-2.0.xsd", SSLmodulusHash)
-	if err.Error() != "Document validation error 1845" {
-		t.Error("Schema not validation failed")
+func TestValidateMetadataWrongSchema(t *testing.T) {
+	// Validate with a wrong schema to verify that it fails
+	err := validateMetadata(metadata, wrongSchema, SSLmodulusHash)
+	// If no error or the wrong error then fail
+	if err == nil || err.Error() != "Document validation error 1845" {
+		t.Errorf("Wrong schema validation failed '%s'", err)
 	}
 }
 
@@ -39,14 +46,15 @@ func TestValidateMetadataWrongDigest(t *testing.T) {
 	dom := gosaml.NewXp(metadata)
 	dom.QueryDashP(nil, "/./ds:Signature/ds:SignedInfo/ds:Reference/ds:DigestValue[1]", "+dPr0aJZ4IF5ovxAe7Uss+xBu0UNdtgoHq9CppyH2Vs=", nil)
 	metadata2 := []byte(dom.Pp())
-	err := validateMetadata(metadata2, "src/github.com/wayf-dk/gosaml/schemas/saml-schema-metadata-2.0.xsd", SSLmodulusHash)
+	err := validateMetadata(metadata2, schemaPath, SSLmodulusHash)
 	if err.Error() != "Signature check failed. Signature digest mismatch, 3c9a81a80e9032f888ba3cc7ac564364c38f283e = 3c9a81a80e9032f888ba3cc7ac564364c38f283e" {
 		t.Errorf("Wrong digest failed '%s'", err)
 	}
 }
 
+// Catch all validation test
 func TestValidateMetadata(t *testing.T) {
-	err := validateMetadata(metadata, "src/github.com/wayf-dk/gosaml/schemas/saml-schema-metadata-2.0.xsd", SSLmodulusHash)
+	err := validateMetadata(metadata, schemaPath, SSLmodulusHash)
 	if err != nil {
 		t.Errorf("Document not valided '%s'", err)
 	}
